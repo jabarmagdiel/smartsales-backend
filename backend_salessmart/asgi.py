1e19ba2cfb444ae6c754e8232e7645c18a52f943
jabarmagdiel <jabarmagdiel@gmail.com>
2025-11-13 08:10:38 -0400

    Update Django settings module for ASGI application

--- backend_salessmart/asgi.py
+++ backend_salessmart/asgi.py
@@ -11,7 +11,7 @@ import os
 import django
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_salessmart.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_salessmart.settings_railway')
 
 django.setup()
 
