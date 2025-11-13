821a8948cb7879ad27278182f5cc45e203b22b5d
jabarmagdiel <jabarmagdiel@gmail.com>
2025-11-13 05:26:54 -0400

    Update CORS settings in settings.py

--- backend_salessmart/settings.py
+++ backend_salessmart/settings.py
@@ -11,7 +11,10 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ['*']  # Temporal para Cloud Run
 
 # CORS
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'https://smartsales-frontend-production.up.railway.app',
+]
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
