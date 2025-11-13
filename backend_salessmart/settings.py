01fc1ba74e98fa4afeca7f3f7e39fd8951f91757
Jabar-magdiel <152967635+Jabar-magdiel@users.noreply.github.com>
2025-11-13 05:41:19 -0400

    cambiar

--- backend_salessmart/settings.py
+++ backend_salessmart/settings.py
@@ -25,6 +25,9 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
 ]
 
+# También permitir todos los orígenes temporalmente para desarrollo
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOW_CREDENTIALS = True
 
 # APLICACIONES INSTALADAS
